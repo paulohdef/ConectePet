4e3b0a6bb6b2c92d128d2d0beb962e08c8861c68
Paulo <paulohdef1@gmail.com>
2022-05-03 00:07:22 -0300

    delete files

--- src/pages/signout.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next'
-
-interface SiginOutPageProps {}
-
-const SiginoutPage: NextPage<SiginOutPageProps> = (props) => {
-  return <div></div>
-}
-
-export default SiginoutPage
