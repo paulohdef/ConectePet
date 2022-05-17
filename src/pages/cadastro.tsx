40756e98bb5d24f26c8005d499da85d797155f21
paulopsbarbosa <paulopatricksb@gmail.com>
2022-05-17 19:15:31 -0300

    teste

--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -42,7 +42,7 @@ const cadastroPage: NextPage<cadastroPageProps> = (props) => {
                    
                     <div>
                       <label className="text-white dark:text-gray-200" htmlFor="passwordConfirmation">Selecione o tipo de vacina</label>
-                      <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                      <select className="block w-full px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
                         <option>V10</option>
                         <option>V8</option>
                         <option>Antirrábica</option>
