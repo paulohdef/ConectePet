a9b940681fc342ce6d9ad84acee840b6874be20d
paulohdef <paulohdef1@gmail.com>
2022-05-31 18:29:35 -0300

    new changes

--- src/pages/cadastroVacina.tsx
+++ src/pages/cadastroVacina.tsx
@@ -24,7 +24,7 @@ const cadastroPage: NextPage<cadastroPageProps> = (props) => {
             <div>
               <section className="max-w-4xl p-6 mx-auto bg-gray-400 rounded-md shadow-md dark:bg-gray-800 mt-20">
                 <h1 className="text-xl font-bold justify-center flex text-white dark:text-white">
-                  Cadastro do PET
+                  Cadastro de Vacina
                 </h1>
                 <form>
                   <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
@@ -33,7 +33,7 @@ const cadastroPage: NextPage<cadastroPageProps> = (props) => {
                         className="text-white dark:text-gray-200"
                         htmlFor="username"
                       >
-                        Digite o Usuário do Tutor
+                        Nome da Vacina
                       </label>
                       <input
                         id="username"
@@ -41,47 +41,39 @@ const cadastroPage: NextPage<cadastroPageProps> = (props) => {
                         className="block w-full px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                       />
                     </div>
-
-                    <div className="flex justify-start mt-2">
-                      <button className="px-6 py-2 leading-5 text-black transition-colors duration-200 transform bg-white rounded-md hover:bg-grey-700 focus:outline-none focus:bg-gray-600">
-                        Buscar
-                      </button>
-                    </div>
-
                     <div>
                       <label
                         className="text-white dark:text-gray-200"
                         htmlFor="passwordConfirmation"
                       >
-                        Selecione o tipo de PET
-                      </label>
-                      <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
-                        <option>Canino</option>
-                        <option>Felino</option>
-                      </select>
-                    </div>
-
-                    <div>
-                      <label
-                        className="text-white dark:text-gray-200"
-                        htmlFor="passwordConfirmation"
-                      >
-                        Selecione o tipo de vacina
+                        Atende o Gênero
                       </label>
                       <select className="block w-full px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
-                        <option>V10</option>
-                        <option>V8</option>
-                        <option>Antirrábica</option>
-                        <option>Gripe Canina</option>
+                        <option>Canino</option>
+                        <option>Felino</option>
+
                       </select>
                     </div>
-
+                    <div>
+                      <label
+                        className="text-white dark:text-gray-200"
+                        htmlFor="passwordConfirmation"
+                      >
+                        Selecione a dose
+                      </label>
+                      <select className="block w-full px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                        <option>Primeira dose</option>
+                        <option>Segunda dose</option>
+                        <option>Terceira dose</option>
+                        <option>Reforço Anualmente</option>
+                      </select>
+                    </div>
                     <div>
                       <label
                         className="text-white dark:text-gray-200"
                         htmlFor="username"
                       >
-                        Digite o ID do PET
+                        Lote
                       </label>
                       <input
                         id="username"
@@ -89,11 +81,49 @@ const cadastroPage: NextPage<cadastroPageProps> = (props) => {
                         className="block w-full px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                       />
                     </div>
-
-                    <div className="flex justify-center mt-6">
-                      <p className="text-white dark:text-gray-200">
-                        PET NÃO ENCONTRADO
-                      </p>
+                    <div>
+                      <label
+                        className="text-white dark:text-gray-200"
+                        htmlFor="username"
+                      >
+                        Fabricante
+                      </label>
+                      <input
+                        id="username"
+                        type="text"
+                        className="block w-full px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        className="text-white dark:text-gray-200"
+                        htmlFor="username"
+                      >
+                        Período de Vacinação
+                      </label>
+                      <br></br>
+                      <label
+                        className="text-white dark:text-gray-200 text-sm"
+                        htmlFor="username"
+                      >
+                        De
+                      </label>
+                      <input
+                        id="username"
+                        type="text"
+                        className="flex w-half px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                      />
+                      <label
+                        className="text-white dark:text-gray-200 text-sm"
+                        htmlFor="username"
+                      >
+                        Até
+                      </label>
+                      <input
+                        id="username"
+                        type="text"
+                        className="flex w-half px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                      />
                     </div>
                   </div>
                   <div className="flex justify-center mt-6">
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,43 +1,117 @@
-import { NextPage } from 'next'
-import Link from 'next/link'
-import Layout from '../components/templates/Layout'
-import Card from '../components/templates/shortcodes/card'
-import Requisicao from '../core/Requisicao'
+import { NextPage } from "next";
+import Link from "next/link";
+import Layout from "../components/templates/Layout";
+import Card from "../components/templates/shortcodes/card";
+import Requisicao from "../core/Requisicao";
+
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { NextRouter, useRouter } from 'next/dist/client/router'
+
+
+import axios from 'axios'
 
 type DashboarPageProps = {
-  requerimento: Requisicao[]
-}
-
+  requerimento: Requisicao[];
+};
 
 const DashboarPage: NextPage<DashboarPageProps> = (props) => {
+
+
+
+  const router : NextRouter = useRouter();
+
+  const { register, handleSubmit } = useForm<any>()
+
+  async function onSubmit(data: any) : Promise<void>{
+    
+    const { username } = data;
+
+    try{
+
+      await axios.post(`http://localhost:3030/login`, {
+        
+        "username":"bob",
+        "password": 123
+      });
+
+      console.log("teste")
+
+      alert("Usuário existe");
+
+    } catch(error){
+
+      console.log(error);
+      alert("Usuário não existe");
+
+    }
+
+  }
+
+
+
 
   return (
     <Layout titulo="Dashboar" subTitulo="Administrar suas informações">
       <div className="pt-4 px-4">
-  
-
-         <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-            <Card className="xl:col-span-3 ">
-
+        <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+          <Card className="xl:col-span-3 ">
             <div>
-          <Link href="/cadastro">
-            <button className="relative bg-gray-400 text-white p-6 rounded text-2xl font-bold overflow-visible">
-              Cadastrar PET
-            </button>
-          </Link>
-          <br /><br /><br />
-          <button className="relative bg-gray-400 text-white p-6 rounded text-2xl font-bold overflow-visible">
-            Cadastrar Vacina
-          </button>
-        </div>
-      
+              <section className="max-w-4xl p-6 mx-auto bg-gray-400 rounded-md shadow-md dark:bg-gray-800 mt-20">
+                <h1 className="text-xl font-bold justify-center flex text-white dark:text-white">
+                  Buscar Tutor
+                </h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                    <div>
+                      <label
+                        className="text-white dark:text-gray-200"
+                        htmlFor="username"
+                      >
+                        Insira o Nome de Usuário do Tutor
+                      </label>
+                      <input
+                        id="username"
+                        type="text"
+                        className="block w-full px-4 py-2 mt-2 text-gray-600 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                        {...register('username')}
+                      />
+                    </div>
+                    <div className="flex justify-start mt-2">
+                      <button type="submit" className="px-6 py-2 leading-5 text-black transition-colors duration-200 transform bg-white rounded-md hover:bg-grey-700 focus:outline-none focus:bg-gray-600">
+                        Buscar
+                      </button>
+                    </div>
+                  </div>
+                </form>
 
+              </section>
+            </div>
+
+            <div className="flex justify-center mt-6">
+              <Link href="/cadastro">
+                <button className=" px-6 py-2 relative bg-gray-400 text-white p-6 rounded text-2xl font-bold overflow-visible">
+                  Cadastrar PET
+                </button>
+              </Link>
+              <br />
+              <br />
+              <br />
+            </div>
+            <div className="flex justify-center mt-6">
+              <br />
+              <br />
+              <br />
+              <Link href="cadastroVacina">
+                <button className=" px-6 py-2 relative bg-gray-400 text-white p-6 rounded text-2xl font-bold overflow-visible">
+                  Cadastrar Vacina
+                </button>
+              </Link>
+            </div>
           </Card>
         </div>
-
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default DashboarPage
+export default DashboarPage;
